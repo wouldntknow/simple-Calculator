4aba5f6b007601407a14db899fde5f0ba82bee5f
wouldntknow <humnauraizee@gmail.com>
2024-07-11 16:39:15 +0500

    Pushing my file to gitHub

---
+++ index.ts
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "Enter first number:", type: "number", name: "firstNumber" },
+{ message: "Enter second number:", type: "number", name: "secondNumber" },
+{
+    message: "Select an option", type: "list", name: "operator", 
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+}
+]);
+console.log(answer)
+
+if(answer.operator === "Addition"){
+  
+    console.log(answer.firstNumber + answer.secondNumber)
+}
+else if(answer.operator === "Subtraction"){
+  
+    console.log(answer.firstNumber - answer.secondNumber)
+}
+else if(answer.operator === "Multiplication"){
+  
+    console.log(answer.firstNumber * answer.secondNumber)
+}
+else if(answer.operator === "Division"){
+  
+    console.log(answer.firstNumber / answer.secondNumber)
+}
+else{
+    console.log("Select a valid option!")
+}
